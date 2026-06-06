ef6e56a9cf7a3850a80d12982a0232821fbef532
Mikhail <mikhailbialouwork@gmail.com>
2026-06-06 23:03:47 +0200

    commit 10

--- web/src/stores/BoardStore.ts
+++ web/src/stores/BoardStore.ts
@@ -48,28 +48,52 @@ export class BoardStore {
   }
 
   async apiUpdateTask(id: TaskId, task: UpdateTaskRequest) {
-    const oldTask = this.tasks
-      .flatMap((col) => col.tasks)
-      .find((_task) => _task.id === id)!;
+    const sourceColumn = this.tasks.find((col) =>
+      col.tasks.some((t) => t.id === id),
+    )!;
+    const oldTask = sourceColumn.tasks.find((t) => t.id === id)!;
+
     const oldTaskCopy = { ...oldTask };
+    const originalIndex = sourceColumn.tasks.indexOf(oldTask);
+
+    const targetColumnId = task.columnId ?? oldTaskCopy.columnId;
+    const targetIndex = task.position ?? originalIndex;
+
+    const hasLayoutChanged =
+      targetColumnId !== oldTaskCopy.columnId || targetIndex !== originalIndex;
 
     Object.assign(oldTask, task);
 
+    if (hasLayoutChanged) {
+      sourceColumn.tasks.splice(originalIndex, 1);
+      const destColumn = this.tasks.find((col) => col.id === targetColumnId)!;
+      destColumn.tasks.splice(targetIndex, 0, oldTask);
+    }
+
     const { data, error } = await api.PATCH("/tasks/{id}", {
-      params: {
-        path: { id },
-      },
+      params: { path: { id } },
       body: task,
     });
 
     if (error) {
-      runInAction(() => Object.assign(oldTask, oldTaskCopy));
+      runInAction(() => {
+        Object.assign(oldTask, oldTaskCopy);
+
+        if (hasLayoutChanged) {
+          const currentColumn = this.tasks.find(
+            (col) => col.id === targetColumnId,
+          )!;
+          const currentIndex = currentColumn.tasks.indexOf(oldTask);
+          currentColumn.tasks.splice(currentIndex, 1);
+
+          sourceColumn.tasks.splice(originalIndex, 0, oldTask);
+        }
+      });
       return error;
     } else {
       runInAction(() => Object.assign(oldTask, data));
     }
   }
-
   async updateTask(id: TaskId, task: UpdateTaskRequest) {
     this.updateState.run(() => this.apiUpdateTask(id, task));
   }
